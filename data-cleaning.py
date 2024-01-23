fad9c1acbfadf48731770a7ab12297ba9fb2dffb
Gabriele Lancione <gabriele.lancione@gmail.com>
2024-01-23 21:23:08 +0100

    added data-cleaning notebook

---
+++ data-cleaning.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+def data_cleaning(df, col_drop=None, col_replace=None, col_replace_with=None, drop_na=False, fill_na=False, fill_na_with=None,
+                  cat_encoding=False, drop_duplicates=False, feature_eng=False):
+    clean_df = None
+    return clean_df
