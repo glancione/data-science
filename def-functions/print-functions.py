fbcdd6cfdfd9eb76437f8321c8850bf5e404a604
Gabriele Lancione <gabriele.lancione@gmail.com>
2024-06-25 20:09:26 +0200

    minor layout fix

--- def-functions/print-functions.py
+++ def-functions/print-functions.py
@@ -14,45 +14,44 @@ def print_title(text):
 	
 	
 	
-def print_metrics_stats(y_train, y_train_pred, y_test, y_pred, modelName = ' '):
+def print_metrics_stats(y_train, y_train_pred, y_test, y_pred, model_name=''):
     
     ### Train Stats
     precision, recall, fscore, support = score(y_train, y_train_pred)
     print('-----------------------------------------------------')
-    print(modelName + 'Train precision score is {}'.format(precision.mean()))
-    print(modelName + 'Train accuracy score is {}'.format(accuracy_score(y_train, y_train_pred)))
-    print(modelName + 'Train recall score is {}'.format(recall.mean()))
-    print(modelName + "Train f1 score is {}".format(f1_score(y_train, y_train_pred, average="weighted"))
+    print(model_name + ' Train precision score is {}'.format(precision.mean()))
+    print(model_name + ' Train accuracy score is {}'.format(accuracy_score(y_train, y_train_pred)))
+    print(model_name + ' Train recall score is {}'.format(recall.mean()))
+    print(model_name + " Train f1 score is {}".format(f1_score(y_train, y_train_pred, average="weighted")))
     try:
-        print(modelName + "Confusion Matrix: {}".format(confusion_matrix(y_train, y_train_pred))
+        print(model_name + " Confusion Matrix: {}".format(confusion_matrix(y_train, y_train_pred)))
     except Exception as e:
         print(e)
-        print('Confusion Matrix not available...')
+        print(model_name + ' Confusion Matrix not available...')
     try:
-        print(modelName + "Classification Report: {}".format(classification_report(y_train, y_train_pred))
+        print(model_name + ' Classification Report: {}'.format(classification_report(y_train, y_train_pred)))
+        print('-----------------------------------------------------')
     except Exception as e:
         print(e)
-        print('Classification Report not available...')
+        print(model_name + ' Classification Report not available...')
         
     ### Test Stats
     print('-----------------------------------------------------')
     precision, recall, fscore, support = score(y_test, y_pred)
-    print(modelName + 'Test precision score is {}'.format(precision.mean()))
-    print(modelName + 'Test accuracy score is {}'.format(accuracy_score(y_test, y_pred)))
-    print(modelName + 'Test recall score is {}'.format(recall.mean()))
-    print(modelName + "Test f1 score is {}".format(f1_score(y_test, y_pred, average="weighted"))
+    print(model_name + ' Test precision score is {}'.format(precision.mean()))
+    print(model_name + ' Test accuracy score is {}'.format(accuracy_score(y_test, y_pred)))
+    print(model_name + ' Test recall score is {}'.format(recall.mean()))
+    print(model_name + " Test f1 score is {}".format(f1_score(y_test, y_pred, average="weighted")))
     print('-----------------------------------------------------')
     try:
-        print(modelName + "Confusion Matrix: {}".format(confusion_matrix(y_test, y_pred))
+        print(model_name + " Confusion Matrix: {}".format(confusion_matrix(y_test, y_pred)))
     except Exception as e:
         print(e)
-        print('Confusion Matrix not available...')
+        print(model_name + ' Confusion Matrix not available...')
     try:
-        print(modelName + "Classification Report: {}".format(classification_report(y_test, y_pred))
+        print(model_name + ' Classification Report: {}'.format(classification_report(y_test, y_pred)))
+        print('-----------------------------------------------------')
     except Exception as e:
         print(e)
-        print('Classification Report not available...')
-	print('-----------------------------------------------------')
+        print(model_name + ' Classification Report not available...')
 
-	
-	
