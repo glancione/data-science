fd97694f7a806345d72fbd620724984cb797a17e
Gabriele Lancione <gabriele.lancione@gmail.com>
2024-08-01 19:17:07 +0200

    added basic function on gridsearchcv-functions

--- def-functions/gridsearchcv-functions.py
+++ def-functions/gridsearchcv-functions.py
@@ -3,3 +3,18 @@
 ########################################
 
 from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+
+
+def runCV(clf, distrib, X_train, y_train, scoring, verbose=1):
+    print('-' * 20)
+    print('GridSearchCV start')
+    print('-' * 20)
+    clf = GridSearchCV(clf_model, distrib, scoring=scoring_CV, verbose=2)
+    search = clf.fit(X_train, y_train)
+    print('-' * 20)
+    print('GridSearchCV completed')
+    print('-' * 20)
+    return search.best_params_
